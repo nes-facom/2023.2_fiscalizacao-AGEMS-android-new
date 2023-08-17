04f4dd198e1ae840748edceb74e2edfbcb663e23
Vagner Filho <vagner.filhopro@gmail.com>
2023-08-16 21:28:42 -0400

    feature: iniciando cadastro de usuário

---
+++ app/src/main/java/com/ufms/nes/fiscalizacaoagems/ui/signup/SignupViewModel.kt
@@ -0,0 +1,13 @@
+package com.ufms.nes.fiscalizacaoagems.ui.signup
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SignupViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is signup Fragment"
+    }
+    val text: LiveData<String> = _text
+}
